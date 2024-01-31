bdd1203ff21649f94f20d75b7034f1f1a2ad5be4
alladin06 <141955470+alladin06@users.noreply.github.com>
2024-01-31 22:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share
 import json
 import math
 import pandas as pd
+from geopy.geocoders import Nominatim
 
 def nearby_hospi():
     if st.checkbox("Get my  location"):
+        geoLoc = Nominatim(user_agent="GetLoc")
         df = pd.read_csv("hospital_directory.csv")
         loc = get_geolocation()
         print(type(loc["coords"]))
@@ -13,7 +15,9 @@ def nearby_hospi():
         print(loc1.keys())
         print(loc1['latitude'])
         print(loc1['longitude'])
+        locname = geoLoc.reverse(f"{loc1['latitude']}, {loc1['longitude']}")
         st.write(f"Your coordinates are {loc1['latitude']},{loc1['longitude']}")
+        st.write(f"You are at {locname.address})
 
         longitude =  float(loc1['longitude'])
         latitude =float(loc1['latitude'])
