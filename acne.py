4883e5f60ceb80c368a149fa303990419fb7ad2d
alladin06 <141955470+alladin06@users.noreply.github.com>
2024-03-16 10:05:08 +0530

    Update acne.py

--- acne.py
+++ acne.py
@@ -59,7 +59,24 @@ def main_acne():
                         except Exception as e:
                             st.error(f"Error loading image {image_path}: {e}")
 
-                    st.balloons()
+                    st.write("""Acne is a common skin condition that occurs when hair follicles become clogged with oil and dead skin cells. 
+                    It often manifests as pimples, blackheads, whiteheads, or cysts, and can occur on the face, neck, chest, back, shoulders, and upper arms. Symptoms of acne can vary in severity and may include:""")
+                    st.write("""1. Pimples: These are raised bumps on the skin that may be red, swollen, and filled with pus. They can appear as whiteheads, which are closed comedones 
+                    with a white center, or blackheads, which are open comedones with a dark surface.""")
+                    st.write("""2. Cysts: These are large, painful, pus-filled lumps beneath the surface of the skin. 
+                    Cysts are a more severe form of acne and can cause scarring if not treated properly.""")
+                    st.write("""3. Nodules: Similar to cysts, nodules are large, painful bumps deep within the skin. They develop when clogged pores become infected and inflamed.""")
+                    st.write("""4. Papules: These are small, red bumps on the skin that may be tender to the touch.
+                    Papules occur when the hair follicles become inflamed due to excess oil and bacteria.""")
+                    st.write("""5. Scarring: Severe acne can lead to permanent scarring, which may be depressed or raised. 
+                    Scarring occurs when the skin tissue is damaged during the healing process.""")
+                    st.write("""6. Oily skin: Excess oil production, known as sebum, is a common symptom of acne. 
+                    Oily skin can contribute to clogged pores and the development of acne lesions.""")
+                    st.write("""7. Inflammation: Acne is often accompanied by redness and inflammation in the affected areas. 
+                    Inflammation occurs as the body's immune response attempts to fight off the bacterial infection within the hair follicles.""")
+                    st.write("""8. Itching or burning: Some individuals may experience itching or burning sensations in areas affected by acne, especially when the skin is inflamed or irritated.""")
+
+
             else:
                 with st.spinner(text='Preparing Video'):
                     for vid in os.listdir(get_detection_folder()):
